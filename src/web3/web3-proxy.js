82bc817cd9f5f68871deb9ed8010f1a00e0ca861
source-code-smith <herman@sourcecodesmith.com>
2018-08-31 13:36:17 +0200

    moved debugger

--- src/web3/web3-proxy.js
+++ src/web3/web3-proxy.js
@@ -13,13 +13,14 @@ export default class Web3Proxy {
     }
 
     initWithCurrentProvider = (currentProvider) => {
+        // const web3 = new Web3(window.web3.currentProvider);
+        // const windowWeb3 = window.web3;
+        // debugger;
+
         this._web3 = new Web3(currentProvider)
     }
 
     getAccount = () => {
-        // const web3 = new Web3(window.web3.currentProvider);
-        // const windowWeb3 = window.web3;
-        // debugger;
         return new Promise((resolve, reject) => {
             const account = (this._web3.eth.accounts.length > 0)?this._web3.eth.accounts[0]:undefined;
             resolve(account);
