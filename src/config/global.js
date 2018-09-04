9f31b29632133ca54d41e1b2a43befd01b9df73e
source-code-smith <herman@sourcecodesmith.com>
2018-09-04 10:20:15 +0200

    local ganache

--- src/config/global.js
+++ src/config/global.js
@@ -3,7 +3,7 @@ const abi = require('./abi.json');
 
 const config = {
   abi: JSON.stringify(abi),
-  contractAddress: '0xbe5cfc105e4078d1a84a252c9009b1f9106296aa',
+  contractAddress: '0x58250bafe2417d3c798f7a124b46ef81fbc185da',
   beneficiaryAddress: '0x0e7cbb02e12ce1421e3b62593da361c1ea41962b',
   desiredNetwork: networks.PRIVATE_NETWORK
 };
